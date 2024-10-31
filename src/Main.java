e6fff258169d4fadaa6032fff558d62cbe33f860
Wilkolak3 <patrykjanusz@onet.pl>
2024-10-31 11:23:36 +0100

    31.10

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        MyList<Integer> list = new MyList<>();
+        System.out.println("Podawaj wartości do wpisania na listę. " +
+                "Wpisanie -1 powoduje koniec wczytywania");
 
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        while(true) {
+            value = scanner.nextInt();
+            if(value == -1){
+                break;
+            }
+            list.addElement(value);
+        }
 
+        System.out.println("-----------");
+        list.display();
     }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -2,6 +2,96 @@ public class MyList<T> {
     private Node<T> head;
 
     public void addElement(T value){
+        Node<T> node = new Node<>(value, null);
+        if(head == null){
+            head = node;
+        }
+        else{
+            Node<T> element = head;
+            while(element.next != null){
+                element = element.next;
+            }
+            element.next = node;
+        }
+    }
 
+    // wyświetl wszystkie elementy listy
+    public void display(){
+        Node<T> node = head;
+        if(node != null){
+            while(node.next != null){
+                System.out.print(node.value);
+                node = node.next;
+            }
+            System.out.println(node.value);
+        }
+    }
+
+    // usuwa ostatni element z listy
+
+    public void removeElement(){
+        Node<T> node = head;
+        if(node != null){
+            Node<T> prev = node;
+            while(node.next != null){
+                prev = node;
+                node = node.next;
+            }
+            if(prev != node){
+                prev.next = null;
+            }
+            else{
+                head = null;
+            }
+        }
+    }
+
+    // wyszukaj element
+    public Node<T> getElement(T value){
+        Node<T> node = head;
+        if (node != null) {
+            while (node.next != null) {
+                if (node.value.equals(value)) {
+                    return node;
+                }
+                node = node.next;
+            }
+        }
+        return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
